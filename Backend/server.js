b280c2a3fa43af6b2919955038e6ca3af3781749
Vikas-Gn <vikasgn2000@gmail.com>
2025-06-19 13:41:58 +0530

    updated-claim-module

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3005;
 
 // PostgreSQL database connection configuration
 const pool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: 'postgres',
     database: 'claims_db',
-    password: 'root',
+    password: 'admin123',
     port: 5432,
 });
 
@@ -164,5 +164,5 @@ app.delete('/api/claims', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://3.108.219.2:${port}`);
 });
